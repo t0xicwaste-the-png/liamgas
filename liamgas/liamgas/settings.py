d4e8a3534a606d4e26cec5212ca5b613da7f2362
Liam Edmonds <liamedmonds@Liams-MacBook-Air.local>
2026-03-05 23:44:06 -0800

    Add Render domains to ALLOWED_HOSTS

--- liamgas/liamgas/settings.py
+++ liamgas/liamgas/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-changethistosomething
 
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,liamgas.xyz,*.onrender.com').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
