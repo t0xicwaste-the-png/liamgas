bafbd41cfab1c2be873865062bc3b0070ff8800e
Liam Edmonds <liamedmonds@Mac.ht.home>
2026-03-12 10:00:59 -0700

    fix app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -14,6 +14,11 @@ if database_url and database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    "pool_pre_ping": True,
+    "pool_recycle": 300,
+}
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
